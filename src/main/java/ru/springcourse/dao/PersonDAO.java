6b989b58b3ddb16a84db423dc5a96981b6dc120c
Okami <mr.naydyuk2@gmail.com>
2025-03-28 16:33:13 +0800

    lesson 21 - CRUD, REST, Паттерн DAO (Data Access Object).

---
+++ src/main/java/ru/springcourse/dao/PersonDAO.java
@@ -0,0 +1,30 @@
+package ru.springcourse.dao;
+
+import org.springframework.stereotype.Component;
+import ru.springcourse.models.Person;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Component
+public class PersonDAO {
+    private static int PEOPLE_COUNT;
+    private List<Person> people;
+
+    {
+        people = new ArrayList<Person>();
+
+        people.add(new Person(++PEOPLE_COUNT, "John"));
+        people.add(new Person(++PEOPLE_COUNT, "Jane"));
+        people.add(new Person(++PEOPLE_COUNT, "Bob"));
+        people.add(new Person(++PEOPLE_COUNT, "Mary"));
+
+    }
+    public List<Person> index() {
+        return people;
+    }
+
+    public Person show(int id) {
+        return people.stream().filter(person -> person.getId() == id).findFirst().orElse(null);
+    }
+}
