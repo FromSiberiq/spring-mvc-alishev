73d29d5a8f8f25beb1641320bd9e5361510b9305
Okami <mr.naydyuk2@gmail.com>
2025-04-03 17:24:52 +0800

    lesson 23 - CRUD приложение. PATCH, DELETE запросы.

--- src/main/java/ru/springcourse/dao/PersonDAO.java
+++ src/main/java/ru/springcourse/dao/PersonDAO.java
@@ -20,16 +20,27 @@ public class PersonDAO {
         people.add(new Person(++PEOPLE_COUNT, "Mary"));
 
     }
+
     public List<Person> index() {
         return people;
     }
 
     public Person show(int id) {
-        return people.stream().filter(person -> person.getId() == id).findFirst().orElse(null);
+        return people.stream().filter(person -> person.getId() == id).findAny().orElse(null);
     }
 
     public void save(Person person) {
         person.setId(++PEOPLE_COUNT);
         people.add(person);
+    }
+
+    public void update(int id, Person updatedPerson) {
+        Person personToBeUpdated = show(id);
+
+        personToBeUpdated.setName(updatedPerson.getName());
+    }
+
+    public void delete(int id) {
+        people.removeIf(p -> p.getId() == id);
     }
 }
